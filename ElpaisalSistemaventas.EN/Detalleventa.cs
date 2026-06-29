a5da8f9393463bc861ce34a189f4c2f360f7b907
Marco Antonio Sanchez <s26001@esfe.agape.edu.sv>
2026-06-29 16:58:46 -0600

    publicar class DetalleVenta con tipos apropriados "get set" y annotaciones

--- ElpaisalSistemaventas.EN/Detalleventa.cs
+++ ElpaisalSistemaventas.EN/Detalleventa.cs
@@ -6,5 +6,17 @@ namespace ElpaisalSistemaventas.EN
 {
     public class Detalleventa
     {
+        public long detalleventaid { get; set; }
+        public long ventaid {  get; set; }
+        public int productoid { get; set; }
+        public decimal precio { get; set; }
+        public short cantidad { get; set; }
+        public decimal subtotal { get; set; }
     }
 }
+//+DetalleVentaId: long
+//+VentaId: long
+//+ProductoId: int
+//+Precio: decimal
+//+Catidad: short
+//+SubTotal: decimal
