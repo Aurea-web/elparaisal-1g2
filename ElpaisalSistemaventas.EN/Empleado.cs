10e45e6015bf3f294a5fba8d51cb590f17b9569c
Kathia Perez <pl26001@esfe.agape.edu.sv>
2026-06-29 17:02:49 -0600

    Codificar Empleado

--- ElpaisalSistemaventas.EN/Empleado.cs
+++ ElpaisalSistemaventas.EN/Empleado.cs
@@ -6,5 +6,12 @@ namespace ElpaisalSistemaventas.EN
 {
     public class Empleado
     {
+        public short EmpleadoId { get; set; }
+         public byte CargoId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Clave { get; set; } = string.Empty;
     }
+
 }
